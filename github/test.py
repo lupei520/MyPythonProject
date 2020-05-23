30a58b6924d577cbeef935dc53acea40384c7967
lupei520 <3028244575@qq.com>
2020-05-23 23:11:45 +0800

    Update test.py

--- github/test.py
+++ github/test.py
@@ -1,1 +1,14 @@
-print('Hello World')
+class figure:
+  from decimal import Decimal
+  import math
+  def __init__(self,a=None,b=None,area=None,c=None):
+    self.a=a
+    self.b=b
+    self.area=area
+    self.c=c
+  def area(self):
+    self.a=self.Decimal(self.a)
+    self.b=self.Decimal(self.b)
+    self.area=self.a*self.b
+    print(self.area)
+    return self.area
